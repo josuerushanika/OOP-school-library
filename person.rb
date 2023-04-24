c6e692bb784e100419201fc512ccbb25f3017ea9
josuerushanika <josuerushanika@gmail.com>
2023-04-24 19:05:54 +0200

    Add attr_accessor and private def of_age?

--- person.rb
+++ person.rb
@@ -1,3 +1,16 @@
 class Person
-    
+    attr_accessor :id, :name, :age
+    def initialize(name = "Unknown", age, parent_permission = true)
+      @id = rand(1..1000)
+      @name = name
+      @age = age
+      @parent_permission = parent_permission
+    end
+  end
+  
+    private
+    def of_age?
+        @age >= 18 ? true : false
+    end
+
 end
