e9f2763af679d9d173db50613504ba0435ed022b
josuerushanika <josuerushanika@gmail.com>
2023-04-26 02:07:51 +0200

    Fix linters

--- person.rb
+++ person.rb
@@ -5,6 +5,8 @@ class Person < Nameable
   attr_accessor :name, :age
 
   def initialize(age, name = 'Unknown', parent_permission: true)
+    super(age, parent_permission)
+
     @id = rand(1..1000)
     @name = name
     @age = age
