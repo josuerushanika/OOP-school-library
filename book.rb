387ee267679aa24f32ca344d39b683874b0c3451
josuerushanika <josuerushanika@gmail.com>
2023-04-27 18:16:21 +0200

    Add book

---
+++ book.rb
@@ -0,0 +1,12 @@
+class Book
+
+    attr_accessor: title, :author, :rentals
+    def initialize(title, author)
+        @title = title
+        @author = author
+    end
+
+    def add_rental(rental)
+        rentals.push(rental)
+    end
+end
