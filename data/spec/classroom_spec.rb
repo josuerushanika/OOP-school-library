a57c256bf7c64481432f7a7698f161282bafca28
josuerushanika <josuerushanika@gmail.com>
2023-05-10 12:57:54 +0200

    Add classroomtest

---
+++ data/spec/classroom_spec.rb
@@ -0,0 +1,20 @@
+require './classroom'
+require './student'
+
+describe Classroom do
+  context 'Creating classroom test methods' do
+    classroom = Classroom.new('Software developer')
+    it 'checks the classroom info' do
+      expect(classroom).to be_a Classroom
+      expect(classroom.label).to eql 'Software developer'
+    end
+
+    it 'adds students to classroom' do
+      new_student = Student.new(24, 'Josue', true)
+      classroom.students.push(new_student)
+      expect(classroom.students).to include(new_student)
+      expect(classroom.students.length).to eql 1
+      expect(new_student.name).to eql 'Josue'
+    end
+  end
+end
