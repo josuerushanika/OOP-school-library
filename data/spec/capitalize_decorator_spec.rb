e8b4a72d955402e1883e91789f94d3884624594c
josuerushanika <josuerushanika@gmail.com>
2023-05-10 15:32:01 +0200

    FIx linters

--- data/spec/capitalize_decorator_spec.rb
+++ data/spec/capitalize_decorator_spec.rb
@@ -2,11 +2,11 @@ require './capitalize_decorator'
 require './person'
 
 describe capitalize_decorator do
-    context 'Capitalize Decorator' do
-        it  'capitalize the first name' do
-            person = Person.new(01, 'Obed')
-            capitalize_person = CapitalizeDecorator.new(person).correct_name
-            expect(capitalize_person).to eq 'Obed'
-        end
+  context 'Capitalize Decorator' do
+    it 'capitalize the first name' do
+      person = Person.new(0o1, 'Obed')
+      capitalize_person = CapitalizeDecorator.new(person).correct_name
+      expect(capitalize_person).to eq 'Obed'
     end
-end            
+  end
+end
